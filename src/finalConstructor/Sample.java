59736d88208bbebec7ec6c84074f7cd44b2ce478
s546652 <98067009+s546652@users.noreply.github.com>
2022-11-05 13:24:54 -0500

    tryBlock

---
+++ src/finalConstructor/Sample.java
@@ -0,0 +1,9 @@
+package finalConstructor;
+public class Sample {
+	   public static void main(String args[]){
+	      int num;
+	      final public Sample(){
+	         num = 30;
+	      }
+	   }
+	}
---
+++ src/tryBlock/TryExample.java
@@ -0,0 +1,18 @@
+package tryBlock;
+
+public class TryExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("without catch block");
+		}
+		/*catch(Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("test finally with only try and finally block");
+		}*/
+	}
+
+}
