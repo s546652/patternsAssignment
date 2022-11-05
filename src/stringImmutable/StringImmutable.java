d9094353840b323d026addf5823b3fe7978c9c20
s546652 <98067009+s546652@users.noreply.github.com>
2022-11-05 17:22:26 -0500

    String immutable

---
+++ src/stringImmutable/StringImmutable.java
@@ -0,0 +1,18 @@
+package stringImmutable;
+import java.io.*;
+class StringImmutable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			String s1 = "Roshni";
+			String s2 =s1.concat(" String immutable");
+
+			System.out.println("s1 refers to " + s1);
+			System.out.println("s2 refers to " + s2);
+		}
+
+
+	}
+
+}
